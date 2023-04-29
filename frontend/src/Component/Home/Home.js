9d54a28be5e2959fb21946a2e4c73dc0225a3e7e
dharmik-1611 <rathoddharmik1990@gmail.com>
2023-04-29 21:26:39 +0530

    added changes file

--- frontend/src/Component/Home/Home.js
+++ frontend/src/Component/Home/Home.js
@@ -4,28 +4,38 @@ import './Home.css';
 import MetaData from '../Layout/MetaData.js';
 import { getProduct } from '../../Actions/productAction.js';
 import { useSelector, useDispatch } from 'react-redux';
+import Loader from '../Layout/Loader/Loader.js';
+import { useAlert } from 'react-alert';
 const Home = () => {
+    const alert = useAlert();
     const dispatch = useDispatch();
-    const { loading, error, product, productCount } = useSelector(state => state.product);
+    const { loading, error, product } = useSelector(state => state.product);
     useEffect(() => {
+        if (error) {
+            return alert.error(error.message);
+        }
         dispatch(getProduct())
-    }, [dispatch]);
+    }, [dispatch, error, alert]);
     return (
         <>
-            <MetaData title="Shoppy Online" />
-            <div className='banner'>
-                <p>Welcome to Shoppy Online</p>
-                <h1>FIND AMAZING PRODUCTS BELOW</h1>
-                <a href="#container">
-                    <button>
-                        Scroll
-                    </button>
-                </a>
-            </div>
-            <h2 className='homeHeading'>Featured Products </h2>
-            <div className="container" id="container">
-                {product && product.map((product) => <Product product={product} />)}
-            </div>
+            {loading ? <Loader /> : (
+                <>
+                    <MetaData title="Shoppy Online" />
+                    <div className='banner'>
+                        <p>Welcome to Shoppy Online</p>
+                        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+                        <a href="#container">
+                            <button>
+                                Scroll
+                            </button>
+                        </a>
+                    </div>
+                    <h2 className='homeHeading'>Featured Products </h2>
+                    <div className="container" id="container">
+                        {Array.isArray(product) && product.map((product) => <Product product={product} />)}
+                    </div>
+                </>
+            )}
         </>
     )
 }
---
+++ frontend/src/Component/Layout/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loading.css'
+const Loader = () => {
+    return (
+        <div className='loading'>
+            <div></div>
+        </div>
+    )
+}
+
+export default Loader
